10c99cbd2ac29494b9e9f3af1989666066177988
RubensSoft <joserubencastrosoriano@gmail.com>
2020-09-26 14:40:18 +0200

    Add popup with State to the button

--- BullsEye-SwiftUI/ContentView.swift
+++ BullsEye-SwiftUI/ContentView.swift
@@ -1,14 +1,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isAlertPresented: Bool = false
+    
     var body: some View {
         VStack {
             Text("Hello, World!")
                 .fontWeight(.semibold)
             Button(action: {
-                print("Hit me again!")
+                self.isAlertPresented = true
+                print("Buton pressed")
             }) {
                 Text("Hit me!!!")
+            }
+            .alert(isPresented: $isAlertPresented) {
+                Alert(title: Text("Hi there!"),
+                      message: Text("Hit me again"),
+                      dismissButton: .default(Text("See you soon")))
             }
         }
     }
