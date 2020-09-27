e0ca9de5268ea95f18fbf9bb08d9b23d6c952155
RubensSoft <joserubencastrosoriano@gmail.com>
2020-09-27 10:35:47 +0200

    Add Spacers

--- BullsEye-SwiftUI/ContentView.swift
+++ BullsEye-SwiftUI/ContentView.swift
@@ -5,38 +5,46 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            Spacer()
+            
             HStack {
                 Text("Put the bullseye as close as you can to:")
                 Text("100")
                     .fontWeight(.bold)
             }
+            Spacer()
             
             HStack {
                 Text("1")
                 Slider(value: .constant(10))
                 Text("100")
             }
+            Spacer()
             
             Button(action: {
-                      self.isAlertPresented = true
-                      print("Buton pressed")
-                  }) {
-                      Text("Hit me!!!")
-                  }
-                  .alert(isPresented: $isAlertPresented) {
-                      Alert(title: Text("Hi there!"),
-                            message: Text("Hit me again"),
-                            dismissButton: .default(Text("See you soon")))
-                  }
+                self.isAlertPresented = true
+                print("Buton pressed")
+            }) {
+                Text("Hit me!!!")
+            }
+            .alert(isPresented: $isAlertPresented) {
+                Alert(title: Text("Hi there!"),
+                      message: Text("Hit me again"),
+                      dismissButton: .default(Text("See you soon")))
+            }
+            Spacer()
             
             HStack {
                 Button(action: {}) {
                     Text("Start over")
                 }
+                Spacer()
                 Text("Score:")
                 Text("999999")
+                Spacer()
                 Text("Round:")
                 Text("999")
+                Spacer()
                 Button(action: {}) {
                     Text("Info")
                 }
