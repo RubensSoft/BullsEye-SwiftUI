3f39f8b3b2db3441bdeffeef66f3ff97a5307abb
RubensSoft <joserubencastrosoriano@gmail.com>
2020-10-11 00:17:22 +0200

    Add About screen content - to enter emojis the keyboard command is: control+command+space

--- BullsEye-SwiftUI/AboutView.swift
+++ BullsEye-SwiftUI/AboutView.swift
@@ -2,7 +2,12 @@ import SwiftUI
 
 struct AboutView: View {
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Text("🎯 BullsEye 🎯")
+            Text("This is Bullseye, the game where you can win points and earn fame by draggin a slider.")
+            Text("Your goal is to place the slider as close as you possible to the target value. Te closer you are, the more points you score.")
+            Text("Enjoy!")
+        }
     }
 }
 
