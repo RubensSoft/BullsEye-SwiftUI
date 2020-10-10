eb133be0cd05a5bec65a84f6f733b674d97aea5a
RubensSoft <joserubencastrosoriano@gmail.com>
2020-10-11 00:24:33 +0200

    Add ViewModifier to About view

--- BullsEye-SwiftUI/AboutView.swift
+++ BullsEye-SwiftUI/AboutView.swift
@@ -4,10 +4,35 @@ struct AboutView: View {
     var body: some View {
         VStack {
             Text("🎯 BullsEye 🎯")
+                .modifier(AboutHeadingStyle())
             Text("This is Bullseye, the game where you can win points and earn fame by draggin a slider.")
+                .modifier(AboutBodyStyle())
             Text("Your goal is to place the slider as close as you possible to the target value. Te closer you are, the more points you score.")
+                .modifier(AboutBodyStyle())
             Text("Enjoy!")
+                .modifier(AboutBodyStyle())
         }
+    }
+}
+
+struct AboutHeadingStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.custom("Arial Rounded MT Bold", size: 16))
+            .foregroundColor(.black)
+            .padding(.top, 20)
+            .padding(.bottom, 20)
+    }
+}
+
+struct AboutBodyStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.custom("Arial Rounded MT Bold", size: 16))
+            .foregroundColor(.black)
+            .padding(.leading, 60)
+            .padding(.trailing, 60)
+            .padding(.bottom, 20)
     }
 }
 
